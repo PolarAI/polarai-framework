7d580cff117b485910369e40b57ab5885c0862e4
Alexander Batashev <alexbatashev@outlook.com>
2019-07-26 17:28:44 +0300

    Unify tests name

--- CMakeModules/AthenaIntegrationTest.cmake
+++ CMakeModules/AthenaIntegrationTest.cmake
@@ -4,9 +4,10 @@ function(add_athena_integration_test)
     cmake_parse_arguments(PARSED_ARGS "" "TARGET_NAME" "SRCS;LIBS" ${ARGN})
     set(INTEG_CONFIG_INNER_NAME "config.yml")
     set(INTEG_CONFIG_OUTER_NAME "${PARSED_ARGS_TARGET_NAME}.yml")
-    add_executable(${PARSED_ARGS_TARGET_NAME} ${modifier} ${PARSED_ARGS_SRCS} $<TARGET_OBJECTS:IntegrationTestFramework>)
+    set(TARGET_NAME TestIntegration${PARSED_ARGS_TARGET_NAME}Runnable)
+    add_executable(${TARGET_NAME} ${modifier} ${PARSED_ARGS_SRCS} $<TARGET_OBJECTS:IntegrationTestFramework>)
 
-    target_link_libraries(${PARSED_ARGS_TARGET_NAME} PRIVATE
+    target_link_libraries(${TARGET_NAME} PRIVATE
             Threads::Threads
             athena
             AthenaDep::googletest-gtest
@@ -15,14 +16,11 @@ function(add_athena_integration_test)
             )
 
     if (PARSED_ARGS_LIBS)
-        target_link_libraries(${PARSED_ARGS_TARGET_NAME} PRIVATE ${PARSED_ARGS_LIBS})
+        target_link_libraries(${TARGET_NAME} PRIVATE ${PARSED_ARGS_LIBS})
     endif ()
     if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${INTEG_CONFIG_INNER_NAME})
         configure_file(${INTEG_CONFIG_INNER_NAME} ${INTEG_CONFIG_OUTER_NAME})
         install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${INTEG_CONFIG_OUTER_NAME} DESTINATION test)
     endif ()
-    add_test(NAME "${PARSED_ARGS_TARGET_NAME}IntegrationTest" COMMAND ${PARSED_ARGS_TARGET_NAME})
-
-    install(TARGETS ${PARSED_ARGS_TARGET_NAME}
-            RUNTIME DESTINATION test)
+    add_test(NAME "${TARGET_NAME}IntegrationTest" COMMAND ${PARSED_ARGS_TARGET_NAME})
 endfunction(add_athena_integration_test)
