32c761aa711501ee85f37ca8b220efac3338c409
Alexander Batashev <alexbatashev@outlook.com>
2019-07-26 14:53:33 +0300

    New dependency management system

--- CMakeModules/AthenaIntegrationTest.cmake
+++ CMakeModules/AthenaIntegrationTest.cmake
@@ -5,14 +5,17 @@ function(add_athena_integration_test)
     set(INTEG_CONFIG_INNER_NAME "config.yml")
     set(INTEG_CONFIG_OUTER_NAME "${PARSED_ARGS_TARGET_NAME}.yml")
     add_executable(${PARSED_ARGS_TARGET_NAME} ${modifier} ${PARSED_ARGS_SRCS} $<TARGET_OBJECTS:IntegrationTestFramework>)
-    find_package(Boost 1.50.0 COMPONENTS filesystem REQUIRED QUIET)
-    find_package(YAMLCPP)
 
-    target_link_libraries(${PARSED_ARGS_TARGET_NAME} ${GTEST_LIBRARY} Threads::Threads athena ${YAMLCPP_LIBRARY})
+    target_link_libraries(${PARSED_ARGS_TARGET_NAME} PRIVATE
+            Threads::Threads
+            athena
+            AthenaDep::googletest-gtest
+            AthenaDep::Boost
+            AthenaDep::yaml-cpp
+            )
 
-    target_link_libraries(${PARSED_ARGS_TARGET_NAME} ${Boost_LIBRARIES})
     if (PARSED_ARGS_LIBS)
-        target_link_libraries(${PARSED_ARGS_TARGET_NAME} ${PARSED_ARGS_LIBS})
+        target_link_libraries(${PARSED_ARGS_TARGET_NAME} PRIVATE ${PARSED_ARGS_LIBS})
     endif ()
     if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${INTEG_CONFIG_INNER_NAME})
         configure_file(${INTEG_CONFIG_INNER_NAME} ${INTEG_CONFIG_OUTER_NAME})
---
+++ dependencies.cmake
@@ -0,0 +1,25 @@
+# This file contains mandatory dependencies
+
+# yaml-cpp is used to parse integration test config files
+AthenaAddDependency(
+        TARGET_NAME yaml-cpp
+        GIT_URL https://github.com/jbeder/yaml-cpp.git
+        GIT_TAG yaml-cpp-0.6.2
+)
+
+# Boost filesystem is used to process paths in integration tests
+AthenaAddDependency(
+        TARGET_NAME Boost
+        PACKAGE Boost
+        COMPONENTS filesystem
+)
+
+# Google test is the primary testing framework for the project
+AthenaAddDependency(
+        TARGET_NAME googletest
+        GIT_URL https://github.com/google/googletest.git
+        GIT_TAG release-1.8.1
+        PACKAGE GTest
+        LIBRARIES gtest gtest_main
+        INCLUDE_PATH googletest/include
+)
