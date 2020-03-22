ddbd8104839c3d98a7f30d99496f06be5cc25efe
Alexander Batashev <alexbatashev@outlook.com>
2020-03-22 14:08:24 +0300

    feat: New Generator deisgn

--- src/core/Generator.cpp
+++ src/core/Generator.cpp
@@ -1,15 +1,15 @@
-/*
- * Copyright (c) 2020 Athena. All rights reserved.
- * https://getathena.ml
- *
- * Licensed under MIT license.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+//===----------------------------------------------------------------------===//
+// Copyright (c) 2020 Athena. All rights reserved.
+// https://getathena.ml
+//
+// Licensed under MIT license.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+//===----------------------------------------------------------------------===//
 
 #include <athena/core/Generator.h>
 
@@ -23,16 +23,17 @@ void Generator::unregisterFunctor(const std::string& name) {
     }
 }
 void Generator::generate(const std::string& functorName,
-                         size_t nodeId,
-                         const std::string& nodeName,
-                         size_t clusterId,
                          const std::vector<inner::Tensor>& args,
                          const std::any& options) {
     if (!hasFunctor(functorName))
         new FatalError(FatalErrorType::ATH_NOT_IMPLEMENTED,
                        "No functor with name ", functorName);
 
-    mRegisteredFunctors[functorName](mContext, mGeneratorState, nodeId,
-                                     nodeName, clusterId, args, options);
+    mRegisteredFunctors[functorName](mContext, mGraphName, mNodeId, mClusterId,
+                                     args, options);
+}
+void Generator::generateLoad(inner::Tensor& target, AbstractLoader* loader) {
+  mRegisteredLoadGenerator(mContext, mGraphName, mNodeId, mClusterId, target,
+                           loader);
 }
 }  // namespace athena::core
