8f4f3c07874bb5ab7cdb16b624c40803797dbb43
Alexander Batashev <alexbatashev@outlook.com>
2020-05-23 12:57:20 +0000

    feat: introduce runtime dialect

---
+++ utils/dialects/include/AthenaRuntime/AthenaRuntimeDialect.h
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+// Copyright (c) 2020 Athena. All rights reserved.
+// https://getathena.ml
+//
+// Licensed under MIT license.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+//===----------------------------------------------------------------------===//
+
+#ifndef ATHENA_ATHENARUNTIMEDIALECT_H
+#define ATHENA_ATHENARUNTIMEDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/StandardTypes.h"
+#include "mlir/IR/TypeSupport.h"
+#include "mlir/IR/Types.h"
+#include "mlir/Parser.h"
+
+namespace mlir::ath_rt {
+
+namespace RuntimeTypes {
+enum Types {
+  Device = mlir::Type::FIRST_PRIVATE_EXPERIMENTAL_9_TYPE,
+  Event
+};
+}
+
+class DeviceType : public Type::TypeBase<DeviceType, Type> {
+public:
+  using Base::Base;
+
+  static bool kindof(unsigned kind) { return kind == RuntimeTypes::Device; }
+
+  static DeviceType get(MLIRContext* context) {
+    return Base::get(context, RuntimeTypes::Device);
+  }
+};
+
+class EventType : public Type::TypeBase<DeviceType, Type> {
+public:
+  using Base::Base;
+
+  static bool kindof(unsigned kind) { return kind == RuntimeTypes::Device; }
+
+  static DeviceType get(MLIRContext* context) {
+    return Base::get(context, RuntimeTypes::Device);
+  }
+};
+
+#include "AthenaRuntime/AthenaRuntimeOpsDialect.h.inc"
+} // namespace mlir::ath_graph
+
+#endif // ATHENA_RUNTIMEDIALECT_H
